79ad5bd61a98f8e836946684f230ca7f204b5ce4
Jorge Calderon <calderon.rjorge@gmail.com>
2024-11-18 12:59:54 -0400

    Modulo DBZ

--- 02-bases/src/app/heroes/heroes.module.ts
+++ 02-bases/src/app/heroes/heroes.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-
 import { HeroComponent } from './hero/hero.component';
 import { HeroesListComponent } from './list/heroes-list.component';
 
